42f13760d66f74657bd9f9954e41d080df107b77
Nessa Scott <nvscott@uci.edu>
2019-04-07 01:56:43 -0700

    Data saved and stores HOORAY

--- background.js
+++ background.js
@@ -1,7 +1,19 @@
+var wishList = [];
+
+//chrome.storage.local.clear();
+
+chrome.storage.local.get('myList', function(myList) {
+    myList = myList['myList'];
+    for (var i = 0; i < myList.length; i++) {
+        wishList.push(myList[i]);
+    }
+    console.log(wishList)
+
+});
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        chrome.storage.local.set({'myList': request}, function() {
-            console.log('Settings saved');
-        });
-        sendResponse(request.name);
+        wishList.push(request);
+        chrome.storage.local.set({'myList': wishList});
+        sendResponse(wishList);
     });
